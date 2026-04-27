852a5492063756b4ccaad68b1f0fd515a2b2b016
Vlan <23ik069si@esisalama.org>
2026-04-27 14:01:10 +0200

    Mise a jour de la liberation du vehicule.

--- app/src/main/java/com/example/parck/ui/screens/ExitScreen.kt
+++ app/src/main/java/com/example/parck/ui/screens/ExitScreen.kt
@@ -21,7 +21,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.common.Icon
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
